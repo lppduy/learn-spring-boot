9165e3bbfb3140b710a14103f5c0786687d557ff
lppduy <lephamphuongduy@gmail.com>
2024-05-19 14:21:07 +0700

    validate input

--- src/main/java/dev/lppduy/learnspringboot/controller/UserController.java
+++ src/main/java/dev/lppduy/learnspringboot/controller/UserController.java
@@ -1,31 +1,33 @@
 package dev.lppduy.learnspringboot.controller;
 
 import dev.lppduy.learnspringboot.dto.request.UserRequestDTO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @PostMapping(value = "/",headers = "apiKey=v1.0")
+    @PostMapping("/")
 //    @RequestMapping(path = "/", method = RequestMethod.POST, headers="apiKey=v1.0")
-    public String addUser(@RequestBody UserRequestDTO user) {
+    public String addUser(@Valid @RequestBody UserRequestDTO user) {
         return "User added";
     }
 
     @PutMapping("/{userId}")
-    public String updateUser(@PathVariable int userId, @RequestBody UserRequestDTO user) {
+    public String updateUser(@PathVariable int userId,@Valid @RequestBody UserRequestDTO user) {
         System.out.println("Update user");
         return "User updated";
     }
 
     @PatchMapping("/{userId}")
-    public String updateStatus(@PathVariable int userId, @RequestParam boolean status) {
+    public String updateStatus(@PathVariable int userId, @RequestParam @Min(1) int status) {
         return "User Status changed";
     }
 
     @DeleteMapping("/{userId}")
-    public String deleteUser(@PathVariable int userId) {
+    public String deleteUser(@PathVariable @Min(1) int userId) {
         return "User deleted";
     }
 
--- src/main/java/dev/lppduy/learnspringboot/dto/request/UserRequestDTO.java
+++ src/main/java/dev/lppduy/learnspringboot/dto/request/UserRequestDTO.java
@@ -1,12 +1,37 @@
 package dev.lppduy.learnspringboot.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import dev.lppduy.learnspringboot.util.PhoneNumber;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 public class UserRequestDTO implements Serializable {
+    @NotBlank(message = "firstName must be not blank") // Khong cho phep gia tri blank
     private String firstName;
+
+    @NotNull(message = "lastName must be not null") // Khong cho phep gia tri null
     private String lastName;
+
+    @Email(message = "email invalid format") // Chi chap nhan nhung gia tri dung dinh dang email
     private String email;
+//    @Pattern(regexp = "^\\d{10}$", message = "phone invalid format")
+    @PhoneNumber
     private String phone;
+
+    @NotNull(message = "dateOfBirth must be not null")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = "MM/dd/yyyy")
+    private Date dateOfBirth;
+
+    @NotEmpty(message = "permissions must be not empty")
+    private List<String> permissions;
 
     // constructor
 
@@ -50,5 +75,21 @@ public class UserRequestDTO implements Serializable {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public List<String> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<String> permissions) {
+        this.permissions = permissions;
     }
 }
